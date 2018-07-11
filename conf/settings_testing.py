31ff5b5928ee3fead4bd7880041c8b4b950bba48
liaow8 <82854738@qq.com>
2018-07-11 13:34:06 +0800

    Signed-off-by: liaow8 <82854738@qq.com>

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -21,7 +21,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                            # 你的数据库user
-        'PASSWORD': '1234',                        # 你的数据库password
+        'PASSWORD': 'zj(4Obp1Z*',                        # 你的数据库password
         'HOST': '172.19.17.7',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
